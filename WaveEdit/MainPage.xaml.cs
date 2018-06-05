57a28adc1d532e99001cead4080135b500312c1b
Maxime Leboeuf <ml@maximeleboeuf.com>
2018-06-04 21:57:57 -0400

    Cleanup MainPage

--- WaveEdit/MainPage.xaml.cs
+++ WaveEdit/MainPage.xaml.cs
@@ -7,13 +7,8 @@ using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace WaveEdit
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public WaveEditor Editor { get; set; }
@@ -23,10 +18,6 @@ namespace WaveEdit
             this.Editor = new WaveEditor();
             this.InitializeComponent();
         }
-
-        public double _pointerX1 { get; set; }
-        public double _pointerX2 { get; set; }
-        public bool IsPointerPressed { get; set; } // Whether the left mouse button is being held down
 
         private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
@@ -70,20 +61,20 @@ namespace WaveEdit
             }
 
             // Selection
-            if (_pointerX1 > 0)
+            if (this.Editor.PointerX1 > 0)
             {
-                args.DrawingSession.DrawLine((float)_pointerX1, 0f, (float)_pointerX1, (float)height, Colors.Blue);
+                args.DrawingSession.DrawLine((float)this.Editor.PointerX1, 0f, (float)this.Editor.PointerX1, (float)height, Colors.Blue);
             }
-            if (_pointerX2 > 0)
+            if (this.Editor.PointerX2 > 0)
             {
-                args.DrawingSession.DrawLine((float)_pointerX2, 0f, (float)_pointerX2, (float)height, Colors.Blue);
+                args.DrawingSession.DrawLine((float)this.Editor.PointerX2, 0f, (float)this.Editor.PointerX2, (float)height, Colors.Blue);
             }
 
-            if (IsPointerPressed)
+            if (this.Editor.IsPointerPressed)
             {
                 var brush = new CanvasSolidColorBrush(canvas, Colors.LightSkyBlue);
                 brush.Opacity = 0.4f;
-                args.DrawingSession.FillRectangle((float)_pointerX1, 0f, (float)(_pointerX2 - _pointerX1), (float)height, brush);
+                args.DrawingSession.FillRectangle((float)this.Editor.PointerX1, 0f, (float)(this.Editor.PointerX2 - this.Editor.PointerX1), (float)height, brush);
             }
 
         }
@@ -91,13 +82,13 @@ namespace WaveEdit
         private void canvas_PointerMoved(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
         {
             var point = e.GetCurrentPoint(this);
-            if (IsPointerPressed)
+            if (this.Editor.IsPointerPressed)
             {
-                _pointerX2 = point.Position.X;
+                this.Editor.PointerX2 = point.Position.X;
             }
             else
             {
-                _pointerX1 = point.Position.X;
+                this.Editor.PointerX1 = point.Position.X;
             }
 
             canvas.Invalidate();
@@ -105,14 +96,14 @@ namespace WaveEdit
 
         private void canvas_PointerPressed(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
         {
-            IsPointerPressed = true;
+            this.Editor.IsPointerPressed = true;
 
             canvas.Invalidate();
         }
 
         private void canvas_PointerReleased(object sender, Windows.UI.Xaml.Input.PointerRoutedEventArgs e)
         {
-            IsPointerPressed = false;
+            this.Editor.IsPointerPressed = false;
 
             canvas.Invalidate();
         }
